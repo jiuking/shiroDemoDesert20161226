a1010b7679bd102b3e4769d63c20c4f2b98d5a28
Bravowhale <Bravowhale@Bravowhale-PC>
2016-08-16 18:21:37 +0800

    BaseTest添加

--- src/test/java/shiro/hjc/demo/BaseTest.java
+++ src/test/java/shiro/hjc/demo/BaseTest.java
@@ -1,5 +1,13 @@
 package shiro.hjc.demo;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
 import org.junit.Before;
 
 import com.hjc.demo.model.Permission;
@@ -58,5 +66,36 @@ public abstract class BaseTest {
         
         roleService.correlationPermissions(r2.getId(), p1.getId());
         roleService.correlationPermissions(r2.getId(), p2.getId());
+        //新增用戶
+        u1 = new User("zhang",password);
+        u2 = new User("li", password);
+        u3 = new User("wu", password);
+        u4 = new User("wang", password);
+        u4.setLocked(Boolean.TRUE);
+        userService.createUser(u1);
+        userService.createUser(u2);
+        userService.createUser(u3);
+        userService.createUser(u4);
+        //5、關聯用戶-角色
+        userService.correlationRoles(u1.getId(), r1.getId());
+    }
+    
+    @After
+    public void tearDown() throws Exception{
+    	//退出時請解除綁定Subject到線程 否則對下次測試造成影響
+    	ThreadContext.unbindSubject();
+    }
+    
+    protected void login(String configFile,String username,String password){
+    	Factory<SecurityManager> factory = new IniSecurityManagerFactory(configFile);
+    	SecurityManager securityManager = factory.getInstance();
+    	SecurityUtils.setSecurityManager(securityManager);
+    	Subject subject = SecurityUtils.getSubject();
+    	UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+    	subject.login(token);
+    }
+    
+    public Subject subject(){
+    	return SecurityUtils.getSubject();
     }
 }
